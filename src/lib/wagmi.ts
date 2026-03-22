c0b0208e6a094147cabf7f77f07fc2721aedb472
Robert <rsft6000@gmail.com>
2026-03-21 23:42:15 -0600

    rpc env variable change

--- src/lib/wagmi.ts
+++ src/lib/wagmi.ts
@@ -7,11 +7,13 @@ const { connectors } = getDefaultWallets({
   projectId: import.meta.env.VITE_WALLETCONNECT_PROJECT_ID ?? 'YOUR_PROJECT_ID',
 });
 
+const fujiRpc = import.meta.env.VITE_FUJI_RPC_URL ?? 'undefined';
+
 export const config = createConfig({
   chains: [avalancheFuji, avalanche, mainnet],
   connectors,
   transports: {
-    [avalancheFuji.id]: http(),
+    [avalancheFuji.id]: http(fujiRpc),
     [avalanche.id]: http(),
     [mainnet.id]: http(),
   },
